44cd67eda9f94795d132711eea0e4c1d5e10edad
James Macharia Kamau <JKAMAU11@safaricom.co.ke>
2023-06-18 18:49:36 +0300

    initial rendering using ejs

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express'); // import the express module to our application
+
+const app = express(); // first instance of the express
+
+app.set('view engine', 'ejs'); //tell express we will be using ejs view template engine
+
+app.get("/", (req, res) => {
+    res.send('Welcome to our Website to learn')
+})
+
+app.get("/home", (req, res)=>{
+    res.render('index')
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+    console.log(`The server is running on port ${PORT}`)
+
+    // create express server
+})
